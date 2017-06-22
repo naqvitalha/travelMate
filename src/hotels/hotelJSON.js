83d1deb92fdaef44cd141103fdcac41f6a15436a
Muskein Singh <muskein.singh@flipkart.com>
2017-06-23 04:17:28 +0530

    json update

--- src/hotels/hotelJSON.js
+++ src/hotels/hotelJSON.js
@@ -11,9 +11,8 @@ var hotelData = {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
             "pricePerNight": "15,000",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
+            "rating": "4.2",
+            "reviews": "375",
             "usp": "Comfortable Stay, 24X7 Hotline, Awesome Rooms",
             "discount": "16% off",
             "mrp": "18,000",
@@ -23,9 +22,80 @@ var hotelData = {
 
         },
         {
+            "imageURL": "http://www.travelzoo.com/blog/wp-content/uploads/2015/10/tzoo.hd_.77317.2214.322886.HotelMousai.jpg",
+            "title": "Hotel Mousai",
+            "pricePerNight": "15,000",
+            "rating": "4.3",
+            "reviews": "35",
+            "mmtAssured": true,
+            "usp": "24X7 Hotline",
+            "discount": "6% off",
+            "mrp": "18,000",
+            "fewRoomsSignal": true,
+            "location": "Singapore Road, Singapore",
+            "type" : "HOTEL_CARD"
+
+        },
+        {
+            "imageURL": "http://hdwallpaperbackgrounds.net/wp-content/uploads/2015/08/fontainebleau-hotel-miami-beach-hd-wallpapers.jpg",
+            "title": "Fountain View Hotel",
+            "pricePerNight": "13,000",
+            "rating": "4.7",
+            "reviews": "456",
+            "mmtAssured": true,
+            "usp": "Comfortable Stay, 24X7 Hotline, Awesome Rooms",
+            "discount": "22% off",
+            "mrp": "29,000",
+            "fewRoomsSignal": true,
+            "location": "Singapore Road, Singapore",
+            "type" : "HOTEL_CARD"
+
+        },
+        {
+            "imageURL": "https://wallpaperscraft.com/image/singapore_casino_hotel_59558_1920x1080.jpg",
+            "title": "Casino Hotel",
+            "pricePerNight": "12,000",
+            "rating": "4.1",
+            "reviews": "21",
+            "mmtAssured": false,
+            "discount": "9% off",
+            "mrp": "7,000",
+            "fewRoomsSignal": true,
+            "location": "Singapore Road, Singapore",
+            "type" : "HOTEL_CARD"
+
+        },
+        {
+            "imageURL": "http://hdwallpaperbackgrounds.net/wp-content/uploads/2015/08/united-states-hotel-hd-wallpapers.jpg",
+            "title": "Florida Hotel",
+            "pricePerNight": "9,000",
+            "rating": "4.3",
+            "reviews": "325",
+            "mmtAssured": true,
+            "discount": "6% off",
+            "mrp": "10,000",
+            "location": "Singapore Road, Singapore",
+            "type" : "HOTEL_CARD"
+
+        },
+        {
+            "imageURL": "http://www.shangri-la.com/uploadedImages/Shangri-la_Hotels/Shangri-La_Hotel,_Dalian/SLDA-Bg-Hotel-Exterior.jpg",
+            "title": "Shanghri La Hotel",
+            "pricePerNight": "18,000",
+            "rating": "4.9",
+            "reviews": "289",
+            "mmtAssured": true,
+            "discount": "1% off",
+            "mrp": "18,000",
+            "fewRoomsSignal": true,
+            "location": "Singapore Road, Singapore",
+            "type" : "HOTEL_CARD"
+
+        },
+        {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "29,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -40,82 +110,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
-            "discount": "16% off",
-            "mrp": "18,000",
-            "fewRoomsSignal": true,
-            "location": "Singapore Road, Singapore",
-            "type" : "HOTEL_CARD"
-
-        },
-        {
-            "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
-            "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
-            "discount": "16% off",
-            "mrp": "18,000",
-            "fewRoomsSignal": true,
-            "location": "Singapore Road, Singapore",
-            "type" : "HOTEL_CARD"
-
-        },
-        {
-            "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
-            "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
-            "discount": "16% off",
-            "mrp": "18,000",
-            "fewRoomsSignal": true,
-            "location": "Singapore Road, Singapore",
-            "type" : "HOTEL_CARD"
-
-        },
-        {
-            "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
-            "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
-            "discount": "16% off",
-            "mrp": "18,000",
-            "fewRoomsSignal": true,
-            "location": "Singapore Road, Singapore",
-            "type" : "HOTEL_CARD"
-
-        },
-        {
-            "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
-            "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
-            "rating": "4.3",
-            "reviews": "35",
-            "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
-            "discount": "16% off",
-            "mrp": "18,000",
-            "fewRoomsSignal": true,
-            "location": "Singapore Road, Singapore",
-            "type" : "HOTEL_CARD"
-
-        },
-        {
-            "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
-            "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "35,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -134,7 +129,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "45,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -149,7 +144,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "4,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -164,7 +159,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "37,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -179,7 +174,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "25,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -194,7 +189,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "29,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -209,7 +204,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "10,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -224,7 +219,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "5,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -239,7 +234,7 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "37,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
@@ -254,14 +249,13 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "89,000",
             "rating": "4.3",
             "reviews": "35",
             "mmtAssured": true,
-            "usp": "Comfortable Stay, 24X7 Hotline",
+            "usp": "24X7 Hotline",
             "discount": "16% off",
             "mrp": "18,000",
-            "fewRoomsSignal": true,
             "location": "Singapore Road, Singapore",
             "type" : "HOTEL_CARD"
 
@@ -269,14 +263,12 @@ var hotelData = {
         {
             "imageURL": "http://www.thefloridahotelorlando.com/var/floridahotelorlando/storage/images/media/images/photo-gallery/hotel-images/florida-hotel-orlando-night/27177-1-eng-US/Florida-Hotel-Orlando-Night.jpg",
             "title": "Florida Hotel",
-            "pricePerNight": "15,000 per night",
+            "pricePerNight": "29,000",
             "rating": "4.3",
             "reviews": "35",
-            "mmtAssured": true,
             "usp": "Comfortable Stay, 24X7 Hotline",
             "discount": "16% off",
             "mrp": "18,000",
-            "fewRoomsSignal": true,
             "location": "Singapore Road, Singapore",
             "type" : "HOTEL_CARD"
 
